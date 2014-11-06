a5d2cdc1ae5efece4c79ed40e79590266921a09c
ja <ja@ja>
2014-11-06 20:28:13 +1100

    rewrote the class based on design using a Use Case

---
+++ src/todo/UserInterface.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package todo;
+
+import java.util.Scanner;
+
+/**
+ * This class takes care of the user interface i.e it helps with displaying of
+ * menus and is the entry point of the application
+ *
+ * @author Jason D'souza student at Monash University
+ *
+ */
+public class UserInterface {
+
+    public void displayMenu() {
+        System.out.println("Menu for ToDo System");
+        System.out.println("Option a: Add Todo");
+        System.out.println("Option b: Remove Todo");
+        System.out.println("Option c: Update Todo");
+        System.out.println("Option d: Generate things due today");
+        System.out.println("Option e: Exit System");
+    }
+
+    /**
+     * Method to scan choice from the keyboard.
+     */
+    private void scanMenuChoice() {
+        Scanner input = new Scanner(System.in);
+        String choice = input.nextLine();
+
+        switch (choice) {
+            case "a":
+                addTodo();
+                break;
+            case "b":
+                removeTodo();
+                break;
+            case "c":
+                updateTodo();
+                break;
+            case "d":
+                generateList();
+                break;
+            case "e":
+                exitSystem();
+                break;
+            default:
+                System.out.println("Please choose the correct option");
+                printMenu();
+                scanMenuChoice();
+                break;
+        }
+    }
+    
+    /**
+     * add item to todolist
+     */
+    public void addTodo()
+    {
+        
+    }
+    
+    /**
+     * removeTodo remove item from todolist
+     */
+    public void removeTodo()
+    {
+    
+    }
+    
+    /**
+     * updateTodo update todo list
+     */
+    public void updateTodo()
+    {
+        
+    }
+    
+    /**
+     * generateList
+     */
+    public void generateList()
+    {
+        
+    }
+    
+    /**
+     * Exit System
+     */
+    public void exitSystem()
+    {
+        
+    }
+   
+    
+
+}
