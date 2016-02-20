1acc74a0ce38e8f9d11b5c73702c6cd9c20568d6
Jason D'souza <saviojason@gmail.com>
2016-02-20 15:37:36 +1100

    cleanup

--- src/todo/Todo.java
+++ src/todo/Todo.java
@@ -5,7 +5,6 @@ package todo;
  * and open the template in the editor.
  */
 
-
 import java.io.File;
 import java.util.ArrayList;
 import java.io.FileNotFoundException;
@@ -19,117 +18,231 @@ import java.util.Scanner;
  */
 public class Todo {
 
-    public ArrayList<Task> tasks = new ArrayList<Task>(); // List of Tasks
-    private Task getDone = new Task(); //Instance of Task
+	public static ArrayList<Task> tasks = new ArrayList<Task>(); // List of Tasks
+	private Task getDone = new Task(); // Instance of Task
 
-    /**
-     * @param args the command line arguments
-     */
-    Todo() {
-        loadTasks();
+	Todo() {
+		loadTasksOnScreen();
 
-    }
+	}
 
-    /**
-     * addTask
-     *
-     * @return tasks
-     */
-    public ArrayList<Task> addTask() {
+	/**
+	 * addTask
+	 *
+	 * @return tasks
+	 */
+	public ArrayList<Task> addTask() {
 
-        // project list of existing tasks
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println(tasks.get(i).getTaskDesc() + "," + tasks.get(i).getDueDate() + "," + tasks.get(i).getPriority() + "\n");
-            //String tmp = tasks.get(i).getTaskDesc() + "," + tasks.get(i).getDueDate() + "," + tasks.get(i).getPriority() + "," + tasks.get(i).getStatus() + "\n";
+		// project list of existing tasks
+		for (int i = 0; i < tasks.size(); i++) {
+			System.out.println(tasks.get(i).getTaskDesc() + "," + tasks.get(i).getDueDate() + ","
+					+ tasks.get(i).getPriority() + "\n");
 
-        }
+			Scanner input = new Scanner(System.in);
+			System.out.println("What needs to get done?");
+			String getMeDone = input.nextLine();
+			System.out.println("Priority");
+			String setPriority = input.nextLine();
+			System.out.println("Due Date");
+			String dueWhen = input.nextLine();
+			this.getDone.setTaskDesc(getMeDone);
+			this.getDone.setDueDate(dueWhen);
+			this.getDone.setPriority(setPriority);
+			this.getDone.setStatus("Not-Done");
 
-        Scanner input = new Scanner(System.in);
-        // Adding new tasks from user        
-        boolean test = false;
-        while (test == false) {
+			tasks.add(this.getDone);
+		}
 
-            System.out.println("What needs to get done?");
-            String getMeDone = input.nextLine();
-            System.out.println("Priority");
-            String setPriority = input.nextLine();
-            System.out.println("Due Date");
-            String dueWhen = input.nextLine();
-            this.getDone.setTaskDesc(getMeDone);
-            this.getDone.setDueDate(dueWhen);
-            this.getDone.setPriority(setPriority);
-            this.getDone.setStatus("Not-Done");
+		return tasks;
 
-            tasks.add(this.getDone);
+	}
 
-            // write to file
-            // get all elements from array list
-            System.out.println("Do you want to continue adding more items to the list? - Y/N");
-            String continueya = input.nextLine();
+	public void addTaskCsvLine(String setTaskDesc, String setDueDate, String setPriority, String setStatus) {
+		getDone.setTaskDesc(setTaskDesc);
+		getDone.setDueDate(setDueDate);
+		getDone.setPriority(setPriority);
+		getDone.setStatus(setStatus);
+	}
 
-            // Y results in program continuing. N results in while loop stopping.
-            if ((continueya.contentEquals("N")) || continueya.contentEquals("n")) {
-                test = true;
-                //Controller write = new Controller();
-                //write.writeFile("Todo.txt");
-            } else {
-                continue;
-            }
+	/**
+	 * removeTodo remove item from todolist
+	 */
+	public static void removeTodo() {
+		System.out.println("What would you like to remove");
+		// following code can be move to a different area as it is probably
+		// being repeated
+		int k = 0;
+		for (int j = 0; j < tasks.size(); j++) {
+			k = j;
+			System.out.println("Task number = " + j);
+			System.out.println(tasks.get(j).getTaskDesc());
+			System.out.println(tasks.get(j).getDueDate());
+			System.out.println(tasks.get(j).getPriority());
+			System.out.println(tasks.get(j).getStatus());
+			System.out.println("");
+			System.out.println("Would you like to remove anything?");
 
-        }
-        return tasks;
-    }
+		}
 
-    /**
-     * Load tasks to memory
-     */
-    public void loadTasks() {
-        // load up list of tasks
-        //Controller reader = new Controller();
-        //reader.fileReader1();// sometimes get an error here about ArrayIndexOutofBoundsException
-//        addTask();
-    	System.out.println("#########################");
-    	System.out.println("Stuff to do");
-    	
+		Scanner input = new Scanner(System.in);
+		String choice = input.nextLine();
 
-        try {
-            Scanner input = new Scanner(System.in);
-//            System.out.print("Enter the file name with extention : ");
-            File file = new File("Todo.txt");
+		if (choice == "y" || choice == "Y") {
+			System.out.println("What item number would you like to remove?");
+			choice = input.nextLine();
+			tasks.remove(k + 1);
 
-            input = new Scanner(file);
+		}
 
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                System.out.println(line);
-            }
-            input.close();
+		if (choice == "n" || choice == "N") {
 
-        } catch (NullPointerException ex) {
-            System.out.println("Todo.txt file not found " + ex);
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        } 
-        
-     System.out.println("");  
+		}
 
-    }
+	}
 
-    public void saveTasks() {
-        try {
-            PrintWriter writer = new PrintWriter("Todo.txt", "UTF-8");
-            for (int i = 0; i < tasks.size(); i++) {
-                Task get = tasks.get(i);
-                String info = get.getDueDate() + " " + get.getPriority() + "" + get.getStatus() + "" + get.getTaskDesc() ;
-                writer.println(info);
-            }
-            
-                        writer.close();
-        } catch (Exception e) {
-            
-            System.out.println(e);
-        }
-    }
+	/**
+	 * Load tasks to memory
+	 */
+	public void loadTasksOnScreen() {
+		// load up list of tasks
+		// Controller reader = new Controller();
+		// reader.fileReader1();// sometimes get an error here about
+		// ArrayIndexOutofBoundsException
+		// addTask();
+		System.out.println("#########################");
+		System.out.println("Stuff to do");
+
+		try {
+			Scanner input = new Scanner(System.in);
+			// System.out.print("Enter the file name with extention : ");
+			File file = new File("Todo.txt");
+
+			input = new Scanner(file);
+
+			while (input.hasNextLine()) {
+				String line = input.nextLine();
+				System.out.println(line);
+			}
+			input.close();
+
+		} catch (NullPointerException ex) {
+			System.out.println("Todo.txt file not found " + ex);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		System.out.println("");
+
+	}
+
+	/**
+	 * 
+	 */
+	public void loadTasksInMemory() {
+
+		try {
+			Scanner input = new Scanner(System.in);
+			// System.out.print("Enter the file name with extention : ");
+			File file = new File("Todo.txt");
+
+			input = new Scanner(file);
+
+			while (input.hasNextLine()) {
+				String line = input.nextLine();
+				String[] splitLine = line.split(",");
+				addTaskCsvLine(splitLine[0], splitLine[1], splitLine[2], splitLine[3]);
+				System.out.println(line);
+			}
+			input.close();
+
+		} catch (NullPointerException ex) {
+			System.out.println("Todo.txt file not found " + ex);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+	
+	/**
+	 * 
+	 */
+
+	public void saveTasks() {
+		try {
+			PrintWriter writer = new PrintWriter("Todo.txt", "UTF-8");
+			for (int i = 0; i < tasks.size(); i++) {
+				Task get = tasks.get(i);
+				String info = get.getDueDate() + " " + get.getPriority() + "" + get.getStatus() + ""
+						+ get.getTaskDesc();
+				writer.println(info);
+			}
+
+			writer.close();
+		} catch (Exception e) {
+
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * removeTodo remove item from todolist
+	 */
+	public static void removeTodo2() {
+		System.out.println("What would you like to remove");
+		// following code can be move to a different area as it is probably
+		// being repeated
+		int k = 0;
+		for (int j = 0; j < tasks.size(); j++) {
+			k = j;
+			System.out.println("Task number = " + j);
+			System.out.println(tasks.get(j).getTaskDesc());
+			System.out.println(tasks.get(j).getDueDate());
+			System.out.println(tasks.get(j).getPriority());
+			System.out.println(tasks.get(j).getStatus());
+			System.out.println("");
+			System.out.println("Would you like to remove anything?");
+
+		}
+
+		Scanner input = new Scanner(System.in);
+		String choice = input.nextLine();
+
+		if (choice == "y" || choice == "Y") {
+			System.out.println("What item number would you like to remove?");
+			choice = input.nextLine();
+			tasks.remove(k + 1);
+
+		}
+
+		if (choice == "n" || choice == "N") {
+
+		}
+	}
+
+	/**
+	 * Remove todo item number
+	 */
+	public static void removeTodoItem() {
+
+		tasks.remove(todonumber);
+
+	}
+
+	/**
+	 * Generate List of todo's. clearly need a for loop
+	 */
+	public static void generateList() {
+		// loop through Todo list and print tasks by number
+
+		for (int j = 0; j < tasks.size(); j++) {
+			System.out.println("Task number = " + j);
+			System.out.println(tasks.get(j).getTaskDesc());
+			System.out.println(tasks.get(j).getDueDate());
+			System.out.println(tasks.get(j).getPriority());
+			System.out.println(tasks.get(j).getStatus());
+			System.out.println("");
+
+		}
+	}
 
 }
